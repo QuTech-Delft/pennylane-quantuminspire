d635e9f1f24a99f589cbc29508ac4c9a55de4bef
QFer <f.j.grooteman@tudelft.nl>
2022-06-13 09:18:49 +0200

    changes ci-files. Moved device test to integration test

--- setup.py
+++ setup.py
@@ -23,9 +23,6 @@ with open("pennylane_quantuminspire/_version.py", "r", encoding="utf-8") as f:
 with open("README.rst", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-# with open("doc/requirements.txt", "r", encoding="utf-8") as fh:
-#     extra_requirements_rtd = fh.read().splitlines()
-
 requirements = [
     "pennylane-qiskit>=0.23.0",
     "quantuminspire>=2.0.0",
