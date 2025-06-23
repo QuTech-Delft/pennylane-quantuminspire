486e5981cf1581aaa14b3afe0c24b1cc779a016d
Nischal Sehrawat <nischal.sehrawat@tno.nl>
2025-06-23 13:05:49 +0200

    Fixed linting error

--- tests/test_cqasm.py
+++ tests/test_cqasm.py
@@ -10,6 +10,7 @@ from pennylane_quantuminspire.qi_instructions import Asm
 with patch("qiskit_quantuminspire.qi_provider.QIProvider"):
     from pennylane_quantuminspire.qi_device import QIDevice
 
+
 def test_dumps(mocker: MockerFixture, QI_backend: QIBackend) -> None:
     device = QIDevice(backend=QI_backend)
 
@@ -19,6 +20,9 @@ def test_dumps(mocker: MockerFixture, QI_backend: QIBackend) -> None:
         Asm("TestBackend", """ a ' " {} () [] b """)
         return qml.expval(qml.PauliX(wires=[0]))
 
-    expected_cqasm = 'version 3.0\n\nqubit[1] q\nbit[1] b\n\nH q[0]\nasm(TestBackend) \'\'\' a \' " {} () [] b \'\'\'\nH q[0]\nbarrier q[0]\nb[0] = measure q[0]\n'
+    expected_cqasm = (
+        "version 3.0\n\nqubit[1] q\nbit[1] b\n\nH q[0]\nasm(TestBackend) ''' a ' \" {} () [] b '''\n"
+        + "H q[0]\nbarrier q[0]\nb[0] = measure q[0]\n"
+    )
 
     assert dumps(quantum_function) == expected_cqasm
